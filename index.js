bee8784592b0a4f37f2cd4ebee224e48a5393b35
kroko <code@kroko.me>
2016-07-02 12:19:35 +0300

    corrected style according to authors @bholloway notes

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ function resolveUrlLoader(content, sourceMap) {
 
   // prefer loader query, else options object, else default values
   var options = defaults(loaderUtils.parseQuery(loader.query), loader.options[camelcase(PACKAGE_NAME)], {
-    absolute        : false,
-    sourceMap       : false,
-    fail            : false,
-    silent          : false,
-    keepQueryInUrl  : false,
-    root            : null
+    absolute  : false,
+    sourceMap : false,
+    fail      : false,
+    silent    : false,
+    keepQuery : false,
+    root      : null
   });
 
   // validate root directory
@@ -194,27 +194,21 @@ function resolveUrlLoader(content, sourceMap) {
         var mod = i % 7;
         if ((mod === 3) || (mod === 5)) {
 
-          // remove query string or hash suffix
-          var uri      = initialised.split(/[?#]/g).shift(),
-              absolute = uri && findFile.absolute(directory, uri, resolvedRoot);
-
-          var hasQuery = false, querySeperator = '';
-          var queryStr = initialised.split(/[?#]/g).pop();
-
-          hasQuery = (queryStr !== uri);
-          if (hasQuery) {
-            querySeperator = (initialised.indexOf('?#') === -1) ? ((initialised.indexOf('?') === -1) ? '#' : '?') : '?#';
-          }
+          // split into uri and query/hash and then find the absolute path to the uri
+          var split    = initialised.split(/([?#])/g),
+              uri      = split[0],
+              absolute = uri && findFile.absolute(directory, uri, resolvedRoot),
+              query    = options.keepQuery ? split.slice(1).join('') : '';
 
           // use the absolute path (or default to initialised)
           if (options.absolute) {
-            return absolute && absolute.replace(BACKSLASH_REGEX, '/').concat((hasQuery && options.keepQueryInUrl) ? querySeperator + queryStr : '') || initialised;
+            return absolute && absolute.replace(BACKSLASH_REGEX, '/').concat(query) || initialised;
           }
           // module relative path (or default to initialised)
           else {
             var relative     = absolute && path.relative(filePath, absolute),
                 rootRelative = relative && loaderUtils.urlToRequest(relative, '~');
-            return (rootRelative) ? rootRelative.replace(BACKSLASH_REGEX, '/').concat((hasQuery && options.keepQueryInUrl) ? querySeperator + queryStr : '') : initialised;
+            return (rootRelative) ? rootRelative.replace(BACKSLASH_REGEX, '/').concat(query) : initialised;
           }
         }
         // everything else, including parentheses and quotation (where present) and media statements
